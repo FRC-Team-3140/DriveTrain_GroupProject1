eb2a2a65c1129c264dfba22d72c2f4fb3e5ec2de
s755905 <s755905@FAR5MR202-04.knoxschools.ad>
2017-11-13 16:20:44 -0500

    I created and updated constants to include drive train values. I updated the OI to return xbox

--- src/main/Constants.java
+++ src/main/Constants.java
@@ -1,4 +1,15 @@
 package main;
 
 public interface Constants {
+	/****************
+	 ****variables***
+	 * for the thing*
+	 ****************
+	 */
+	//copying? What copying, Alex?
+	public final int Xbox_Port=0;
+	public final int LEFT_Drive_Master = 2;
+	public final int LEFT_Drive_Slave1 = 3;
+	public final int RIGHT_Drive_Master = 5;
+	public final int RIGHT_Drive_Slave1 = 6;
 }
