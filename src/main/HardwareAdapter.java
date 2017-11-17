2a27de16d1730f22eb7c369749ab8eefa7fe1d74
TheSuperDuck <TheSuperDuck@users.noreply.github.com>
2017-11-16 19:35:22 -0500

    Changed imports so they work.

--- src/main/HardwareAdapter.java
+++ src/main/HardwareAdapter.java
@@ -1,16 +1,16 @@
 package main;
 
-import lib.joystick.AdvJoystick;
+import com.ctre.CANTalon;
+
 import lib.joystick.XboxController;
 
 public interface HardwareAdapter extends Constants {
-	//oi
+	//OI
 	public static XboxController xbox = new XboxController(Xbox_Port);
-		
+	
 	//DriveTrain
 	public static CANTalon leftDriveMaster = new CANTalon(LEFT_Drive_Master);
 	public static CANTalon leftDriveSlave1 = new CANTalon(LEFT_Drive_Slave1);
-	
 	public static CANTalon rightDriveMaster = new CANTalon(RIGHT_Drive_Master);
-	public static CANTalon rightDriveSlave1 = new CANTalon(RIGHT_Drive_Master);
+	public static CANTalon rightDriveSlave1 = new CANTalon(RIGHT_Drive_Slave1);
 }
