bdf0b7036b4bd19a1c716ca677f41e4faf208f9e
far5guest <far5guest@FAR5MR202-05.knoxschools.ad>
2017-11-16 19:30:15 -0500

    i add Hardwaradapter

--- src/main/HardwareAdapter.java
+++ src/main/HardwareAdapter.java
@@ -1,5 +1,16 @@
 package main;
 
+import lib.joystick.AdvJoystick;
+import lib.joystick.XboxController;
 
 public interface HardwareAdapter extends Constants {
+	//oi
+	public static XboxController xbox = new XboxController(Xbox_Port);
+		
+	//DriveTrain
+	public static CANTalon leftDriveMaster = new CANTalon(LEFT_Drive_Master);
+	public static CANTalon leftDriveSlave1 = new CANTalon(LEFT_Drive_Slave1);
+	
+	public static CANTalon rightDriveMaster = new CANTalon(RIGHT_Drive_Master);
+	public static CANTalon rightDriveSlave1 = new CANTalon(RIGHT_Drive_Master);
 }
