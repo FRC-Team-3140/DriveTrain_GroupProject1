60023cc417d35df24a35fc3b36db8ee6d7c63075
s755905 <s755905@FAR5MR202-04.knoxschools.ad>
2017-11-20 16:22:00 -0500

    Added Talon Constants

--- src/main/Constants.java
+++ src/main/Constants.java
@@ -12,4 +12,17 @@ public interface Constants {
 	public final int LEFT_Drive_Slave1 = 3;
 	public final int RIGHT_Drive_Master = 5;
 	public final int RIGHT_Drive_Slave1 = 6;
+	// DEFAULT TALON MODES
+	public final TalonControlMode DEFAULT_CTRL_MODE = TalonControlMode.PercentVbus;
+	public final boolean DEFAULT_BRAKE_MODE = true;
+	// TALON CONTROL MODES
+	public final TalonControlMode VELOCITY = TalonControlMode.Speed;
+	public final TalonControlMode PERCENT_VBUS_MODE = TalonControlMode.PercentVbus;
+	public final TalonControlMode POSITION = TalonControlMode.Position;
+	public final TalonControlMode VOLTAGE_MODE = TalonControlMode.Voltage;
+	public final TalonControlMode SLAVE_MODE = TalonControlMode.Follower;
+	public final TalonControlMode DISABLED = TalonControlMode.Disabled;
+	// TALON BRAKE MODES
+	public final boolean BRAKE_MODE = true;
+	public final boolean COAST_MODE = false;
 }
